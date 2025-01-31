104d252343e51745dfa98c7437f40171c3625715
Gustavo Rodríguez <ga.rodriguezh@uea.edu.ec>
2025-01-31 11:50:16 -0500

    Proyecto POO

--- Dashboard.py
+++ Dashboard.py
@@ -7,7 +7,10 @@ def mostrar_menu():
         print("1. Semana 2: Técnicas de programación")
         print("2. Semana 3: POO frente a la programación tradicional")
         print("3. Semana 4: Caracteristicas de la POO - Ejemplos del Mundo Real")
-        print("4. Salir")
+        print("4. Semana 5: Tipos de datos, Identificadores")
+        print("5. Semana 6: Objetos, clases, Herencia, Polimorfismo")
+        print("6. Semana 7: Constructores y Destructores")
+        print("7. Salir")
 
         opcion = input("Seleccione una opción: ")
 
@@ -18,6 +21,12 @@ def mostrar_menu():
         elif opcion == "3":
             menu_semana_4()
         elif opcion == "4":
+            menu_semana_5()
+        elif opcion == "5":
+            menu_semana_6()
+        elif opcion == "6":
+            menu_semana_7()
+        elif opcion == "7":
             print("Saliendo del programa...")
             break
         else:
@@ -91,6 +100,42 @@ def menu_semana_4():
             print("Opción no válida. Intente nuevamente.")
 
 
+def menu_semana_5():
+    while True:
+        print("\nSemana 5: Tipos de datos, Identificadores")
+        print("1. Mostrar código del programa: Sistema de Registro")
+        print("2. Ejecutar programa: Sistema de Registro")
+        print("3. Volver al menú principal")
+
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            mostrar_codigo_semana_5()
+        elif opcion == "2":
+            ejecutar_programa_semana_5()
+        elif opcion == "3":
+            break
+        else:
+            print("Opción no válida. Intente nuevamente.")
+
+
+def menu_semana_6():
+    while True:
+        print("\nSemana 6: Objetos, clases, Herencia, Polimorfismo")
+        print("1. Mostrar código del programa: Tienda_Ropa_POO")
+        print("2. Ejecutar programa: Tienda_Ropa_POO")
+        print("3. Volver al menú principal")
+
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            mostrar_codigo_semana_6()
+        elif opcion == "2":
+            ejecutar_programa_semana_6()
+        elif opcion == "3":
+            break
+        else:
+            print("Opción no válida. Intente nuevamente.")
 
 
 
@@ -159,6 +204,31 @@ def mostrar_codigo_semana_4(opcion):
             print("Error: No se pudo leer el archivo")
 
 
+def mostrar_codigo_semana_5():
+    ruta_archivo = os.path.join("Semana 5/Tipos de datos, Identificadores", "Sistema_Registro.py")
+    try:
+        with open(ruta_archivo, "r") as archivo:
+            contenido = archivo.read()
+            print("\n# Código: Sistema_Registro")
+            print(contenido)
+    except FileNotFoundError:
+        print(f"Error: No se encontró el archivo en la ruta {ruta_archivo}")
+    except IOError:
+        print("Error: No se pudo leer el archivo")
+
+
+def mostrar_codigo_semana_6():
+    ruta_archivo = os.path.join("Semana 6/Objetos, clases, Herencia, Polimorfismo", "Tienda_Ropa_POO.py")
+    try:
+        with open(ruta_archivo, "r") as archivo:
+            contenido = archivo.read()
+            print("\n# Código: Tienda_Ropa_POO")
+            print(contenido)
+    except FileNotFoundError:
+        print(f"Error: No se encontró el archivo en la ruta {ruta_archivo}")
+    except IOError:
+        print("Error: No se pudo leer el archivo")
+
 def ejecutar_programa_semana_2():
     ruta_archivo = os.path.join("Semana 2/Técnicas de Programación", "Técnicas de Programación.py")
     try:
@@ -206,6 +276,25 @@ def ejecutar_programa_semana_4(opcion):
         except FileNotFoundError:
             print(f"Error: No se encontró el archivo en la ruta {ruta_archivo}")
 
+
+def ejecutar_programa_semana_5():
+    ruta_archivo = os.path.join("Semana 5/Tipos de datos, Identificadores", "Sistema_Registro.py")
+    try:
+        subprocess.run(["python", ruta_archivo], check=True)
+    except subprocess.CalledProcessError:
+        print("Error: Hubo un problema al ejecutar el programa")
+    except FileNotFoundError:
+        print(f"Error: No se encontró el archivo en la ruta {ruta_archivo}")
+
+
+def ejecutar_programa_semana_6():
+    ruta_archivo = os.path.join("Semana 6/Objetos, clases, Herencia, Polimorfismo", "Tienda_Ropa_POO.py")
+    try:
+        subprocess.run(["python", ruta_archivo], check=True)
+    except subprocess.CalledProcessError:
+        print("Error: Hubo un problema al ejecutar el programa")
+    except FileNotFoundError:
+        print(f"Error: No se encontró el archivo en la ruta {ruta_archivo}")
 
 
 if __name__ == "__main__":
